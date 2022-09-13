973853d0a093a17473cd04ac6589b674422314ee
nozer <93734919+nozer-p@users.noreply.github.com>
2022-09-13 03:35:32 +0300

    move_v5

--- Rolling Ball/Assets/Scripts/Player.cs
+++ Rolling Ball/Assets/Scripts/Player.cs
@@ -16,10 +16,16 @@ public class Player : MonoBehaviour
     private float forceFriction;
     private float g = 9.81f;
 
+    [SerializeField] private Material red;
+    [SerializeField] private Material def;
+    private float timeBtwGoals;
+    [SerializeField] private float startTimeBtwGoals;
+
     private void FixedUpdate()
     {
         Move();
         Ray();
+        ChangeColor();
     }
 
     public void Move()
@@ -59,7 +65,33 @@ public class Player : MonoBehaviour
             Vector3 reflectDir = Vector3.Reflect(ray.direction, hit.normal);
             float rot = 90 - Mathf.Atan2(reflectDir.z, reflectDir.x) * Mathf.Rad2Deg;
             transform.eulerAngles = new Vector3(0f, rot, 0f);
+
+            //if (rot > 90 && rot <= 180) transform.localScale = new Vector3(reflectDir.x, transform.localScale.y, reflectDir.y);
+            //if (rot >= 0 && rot <= 90) transform.localScale = new Vector3(reflectDir.z, transform.localScale.y, reflectDir.x);
+            //else transform.localScale = Vector3.one;
+            //Debug.Log(Mathf.Abs(Mathf.Cos(rot)));
+            
+            //rot = Mathf.Abs(Mathf.Cos(rot));
+            //float rotX = rot;
+            //float rotZ = 1 - rot;
+            //transform.localScale = new Vector3(rotZ * 2, transform.localScale.y, rotX * 2);
+            
+            timeBtwGoals = startTimeBtwGoals;
+            
             Instantiate(prefabSmoke, transform.position, Quaternion.Euler(0f, hit.transform.eulerAngles.y + 90, 0f));
+        }
+    }
+
+    private void ChangeColor()
+    {
+        if (timeBtwGoals > 0)
+        {
+            skin.gameObject.GetComponent<MeshRenderer>().material = red;
+            timeBtwGoals -= Time.deltaTime;
+        }
+        else
+        {
+            skin.gameObject.GetComponent<MeshRenderer>().material = def;
         }
     }
 }
